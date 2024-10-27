e854d710861d7d41798831226ecbf177eb24a125
tienv2i <tienv2i@gmail.com>
2024-10-27 21:47:36 +0700

    add private folder hidden

--- uploader/views.py
+++ uploader/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from filer.models import File, Folder
 from .forms import FileUploadForm
+from django.http import HttpResponseNotFound
 
 def show_folder(request, folder_id=None):
     
     folder = None if folder_id == None else get_object_or_404(Folder, id=folder_id)
     
     if folder is None:
-        subfolders = Folder.objects.filter(parent__isnull=True).order_by('name')
+        subfolders = Folder.objects.filter(parent__isnull=True).exclude(name="Private").order_by('name')
     else:
-        subfolders = Folder.objects.filter(parent=folder).order_by('name')
+        # if folder.name == "Private":
+        #     return HttpResponseNotFound("not found")
+        subfolders = Folder.objects.filter(parent=folder).exclude(name="Private").order_by('name')
         
     files = File.objects.filter(folder=folder).order_by('name')
     
